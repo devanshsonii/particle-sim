1b0ca21aa01286763b8da655025e6256dc468593
devanshsonii <devanshsoni874@gmail.com>
2024-06-16 16:32:57 +0530

    Sand and water logics working as intended, added random movement to water. Need to refactor and clean up the codebase and implement interactions between particles.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "raylib.h"
 #include "grid.h"
 #include <vector>
+#include <random>
 
 using namespace std;
 
@@ -48,11 +49,16 @@ void Draw() {
         }
     }
 }
-
 void Update(){
+    std::random_device rd; // Non-deterministic generator
+    std::mt19937 gen(rd()); // To seed mersenne twister.
+    std::uniform_int_distribution<> distrib(0, 1); // Define range
+
+
     if(IsKeyPressed(KEY_ENTER)){
         isSand = !isSand;
     }
+
     for(int i = numR - 1; i >= 0; i--){
         for(int j = numC - 1; j >= 0; j--){
             cells[i][j].updated = false;
@@ -74,20 +80,18 @@ void Update(){
                     }
                 }
             } 
-            else if(cells[i][j].particleType == WATER && cells[i][j].updated == false){
-                if(i < cells.size() - 1){
-                    if(cells[i+1][j].particleType == EMPTY){
-                        switchCells(cells[i][j], cells[i+1][j]);
-                    } else if(j < cells[0].size() - 1 && cells[i+1][j+1].particleType == EMPTY){
-                        switchCells(cells[i][j], cells[i+1][j+1]);
-                    } else if(j > 0 && cells[i+1][j-1].particleType == EMPTY){
-                        switchCells(cells[i][j], cells[i+1][j-1]);
-                    } else if(j < cells[0].size() - 1 && cells[i][j+1].particleType == EMPTY){
+            if(cells[i][j].particleType == WATER && cells[i][j].updated == false){
+                if(i < cells.size() - 1 && cells[i+1][j].particleType == EMPTY){
+                    switchCells(cells[i][j], cells[i+1][j]);
+                } else {
+                    bool moveLeft = distrib(gen) == 0; // Randomly choose left or right
+                    if(moveLeft && j > 0 && cells[i][j-1].particleType == EMPTY){
+                        switchCells(cells[i][j], cells[i][j-1]);
+                    } else if(!moveLeft && j < cells[0].size() - 1 && cells[i][j+1].particleType == EMPTY){
                         switchCells(cells[i][j], cells[i][j+1]);
                     }
                 }
                 cells[i][j].updated = true;
-                
             }
         }
     }
